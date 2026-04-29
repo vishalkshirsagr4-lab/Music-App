69df3c1f7dc6cc0fa1ee71a70eec57c13925100e
vishal <vishalkshirsagr4@gmail.com>
2026-04-29 21:10:37 +0530

    fixed

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,8 +1,8 @@
 const nodemailer = require("nodemailer");
 
 function createTransporter() {
-  const user = process.env.EMAIL_USER;
-  const pass = process.env.EMAIL_PASS;
+  const user = 'kshirasagarvishal1@gmail.com';
+  const pass = 'puivqujhmgwntgbe';
 
   console.log("🔵 EMAIL_USER:", user);
   console.log("🔵 EMAIL_PASS EXISTS:", !!pass);
