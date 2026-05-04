46333ea537371860ef2dc800b09e293a8098ce77
vishal <vishalkshirsagr4@gmail.com>
2026-05-04 23:09:55 +0530

    backend fix

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 async function startServer() {
   try {
